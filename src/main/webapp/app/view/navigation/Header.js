15e5399018ce57697e02f3b09881d210fd10cc65
Ralph Schaer <ralphschaer@gmail.com>
2011-07-14 08:16:32 +0200

    extracted header into his own component

---
+++ src/main/webapp/app/view/navigation/Header.js
@@ -0,0 +1,41 @@
+Ext.define('E4ds.view.navigation.Header', {
+	alias: 'widget.header',
+	extend: 'Ext.container.Container',
+	height: 35,
+	layout: {
+		type: 'hbox',
+		align: 'stretch'
+	},
+
+	items: [ {
+		html: 'e4ds-template v 0.0.1',
+		cls: 'appHeader'
+	}, {
+		xtype: 'tbspacer',
+		flex: 1
+	}, {
+		xtype: 'label',
+		text: 'Logged on: ',
+		style: 'white-space: nowrap',
+		margins: {
+			top: 6,
+			right: 0,
+			bottom: 0,
+			left: 0
+		}
+	}, {
+		xtype: 'tbspacer',
+		width: 20,
+	}, {
+		xtype: 'button',
+		text: 'Logout',
+		href: '/j_spring_security_logout',
+		target: '_self',
+		margins: {
+			top: 2,
+			right: 0,
+			bottom: 10,
+			left: 0
+		}
+	} ]
+});
--- src/main/webapp/login.js
+++ src/main/webapp/login.js
@@ -2,40 +2,61 @@ Ext.onReady(function() {
 	Ext.QuickTips.init();
 
 	var login = Ext.create('Ext.form.Panel', {
-		frame : true,
-		title : 'Please Login',
-		url : 'j_spring_security_check',
-		width : 300,
-		
-		renderTo : Ext.getBody(),
-		
-		standardSubmit : true,
-		
-		defaults : {
-			anchor : '100%'
+		frame: true,
+		title: 'Please Login',
+		url: 'j_spring_security_check',
+		width: 300,
+		margin: "60px, 0, 0, 100px",
+		renderTo: Ext.getBody(),
+
+		standardSubmit: true,
+
+		defaults: {
+			anchor: '100%'
 		},
-		defaultType : 'textfield',
-		items : [ {
-			fieldLabel : 'Username',
-			name : 'j_username',
-			allowBlank : false
+		defaultType: 'textfield',
+		items: [ {
+			fieldLabel: 'Username',
+			name: 'j_username',
+			focus: true,
+			allowBlank: false
 		}, {
-			fieldLabel : 'Password',
-			name : 'j_password',
-			allowBlank : false
+			fieldLabel: 'Password',
+			name: 'j_password',
+			inputType: 'password',
+			allowBlank: false
 		} ],
-		buttons : [ {
-			text : 'Login',
+		buttons: [ {
+			text: 'Login with user',
 			handler: function() {
-	            var form = this.up('form').getForm();
-	            if (form.isValid()) {
-	                form.submit();
-	            }
-	        }			
+				var form = this.up('form').getForm();
+				form.setValues({
+					j_username: 'user',
+					j_password: 'user'
+				});
+				form.submit();
+			}
+		}, {
+			text: 'Login with admin',
+			handler: function() {
+				var form = this.up('form').getForm();
+				form.setValues({
+					j_username: 'admin',
+					j_password: 'admin'
+				});
+				form.submit();
+			}
+		}, {
+			text: 'Login',
+			handler: function() {
+				var form = this.up('form').getForm();
+				if (form.isValid()) {
+					form.submit();
+				}
+			}
 		} ]
 	});
-	
+
 	login.show();
-	login.center();
 
 });
