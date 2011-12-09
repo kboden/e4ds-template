98a5511e7675fdc98b6701930abe981f7a45b445
Ralph Schaer <ralphschaer@gmail.com>
2011-12-09 10:26:58 +0100

    removed Filter. no longer necessary for spring 3.1.0.rc2

--- src/main/java/ch/ralscha/e4ds/config/ComponentConfig.java
+++ src/main/java/ch/ralscha/e4ds/config/ComponentConfig.java
@@ -1,12 +1,11 @@
 package ch.ralscha.e4ds.config;
 
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@ComponentScan(basePackages = { "ch.ralscha.extdirectspring", "ch.ralscha.e4ds" }, excludeFilters = { @Filter(Configuration.class) })
+@ComponentScan(basePackages = { "ch.ralscha.extdirectspring", "ch.ralscha.e4ds" })
 @PropertySource({ "default.properties", "version.properties" })
 public class ComponentConfig {
 	//nothing here
