e706fdcd8f345140af8ee5c79e34d35c170a1574
Ralph Schaer <ralphschaer@gmail.com>
2012-02-28 05:22:46 +0100

    added development sytem property

--- src/main/java/StartTomcat.java
+++ src/main/java/StartTomcat.java
@@ -2,6 +2,8 @@ import ch.ralscha.embeddedtc.EmbeddedTomcat;
 
 public class StartTomcat {
 	public static void main(String[] args) throws Exception {
+		System.setProperty("spring.profiles.active", "development");
+		
 		EmbeddedTomcat et = new EmbeddedTomcat();
 		et.startAndWait();
 	}
